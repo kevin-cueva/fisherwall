885d1308a04862d39ce333872cf9f84a9d33d387
kevin-cueva <unimagneill@gmail.com>
2022-05-17 17:36:51 -0500

    Cambios echos a las tarjetas de los planes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-
+import Planes from './components/Planes';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
       <h1>Fisherwall</h1>
+      <Planes></Planes>
     </div>
   )
 }
---
+++ src/components/Planes.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import "./Planes.css"
+const Planes = ()=>{
+  const planes = [
+    {'nombre':'Basico',
+     'descripcion':'descripcion del plan basico',
+     'valor':'5 $'},
+     {'nombre':'Intermedio',
+      'descripcion':'descripcion del plan Intermedio',
+      'valor':'10 $'},
+     {'nombre':'Premiun',
+      'descripcion':'descripcion del plan Premiun',
+      'valor':'15 $'},
+     {'nombre':'Empresarial',
+      'descripcion':'descripcion del plan Empresarial',
+      'valor':'A convenir $'},
+  ]
+
+  return(
+    <>
+      <div className="container">
+          {planes.map((item) =>
+            <>
+                <div className="card">
+                  <div className="nombre">{item.nombre}</div>
+                  <div className="descripcion">{item.descripcion}</div>
+                  <div className="valor">{item.valor}</div>
+                </div>
+            </>
+          )}
+      </div>
+    </>
+  );
+}
+
+export default Planes;
